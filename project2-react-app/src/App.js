9c9f664ce110c862d0a54a33c8bbfa5b3f49f149
aysuazammadova <118888443+aysuazammadova@users.noreply.github.com>
2025-01-06 18:00:23 +0400

    Adding new features (2a), Recipe Structure

--- project2-react-app/src/App.js
+++ project2-react-app/src/App.js
@@ -10,6 +10,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/contact" element={<ContactMe />} />
+          <Route path="/recipes" element={<RecipePage />} />
         </Routes>
       </div>
     </Router>
